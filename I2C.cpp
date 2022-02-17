5748ba3b3c117f2c5ed5068a6741e3726c667884
EduardoCavValenca <eduardo.cvalenca@gmail.com>
2022-02-17 10:16:18 -0300

    After cube IDE test

--- I2C.cpp
+++ I2C.cpp
@@ -13,14 +13,12 @@ I2C::~I2C() {
 
 
 uint8_t *I2C::ler(uint16_t tam_registrador, uint16_t registrador, uint16_t tamanho) {
-    uint8_t dados[tamanho];
-	if(HAL_I2C_Mem_Read(this->handler, this->endereco, registrador, tam_registrador, dados, tamanho, this->delay) == HAL_OK)
-        return dados;
-    else    
-        throw 0; //Resolve exception
+    uint8_t *dados = (uint8_t*)malloc(tamanho * sizeof(uint8_t));
+	HAL_I2C_Mem_Read(this->handler, this->endereco, registrador, tam_registrador, dados, tamanho, this->delay);
+	return dados;
 }
 
 
 bool I2C::escrever(uint16_t tam_registrador, uint16_t registrador, uint16_t tamanho, uint8_t* dados) {
-    return (HAL_I2C_Mem_Write(this->handler, this->endereco, registrador, tam_registrador, dados, tamanho, this->delay) == HAL_OK)
+    return (HAL_I2C_Mem_Write(this->handler, this->endereco, registrador, tam_registrador, dados, tamanho, this->delay) == HAL_OK);
 }
--- I2C.h
+++ I2C.h
@@ -1,6 +1,8 @@
 #ifndef I2C_H
 #define I2C_H
 
+#include "main.h"
+#include <stdlib.h>
 #include <stdint.h>
 
 class I2C {
@@ -10,7 +12,7 @@ class I2C {
         uint16_t endereco;
 
     public:
-        I2C(I2C_HandleTypeDef* handler, uint16_t endereco, uint32_t delay); //Definir i2c_config
+        I2C(I2C_HandleTypeDef* handler, uint16_t endereco, uint32_t delay);
         ~I2C();
         uint8_t *ler(uint16_t tam_registrador, uint16_t registrador, uint16_t tamanho);
         bool escrever(uint16_t tam_registrador, uint16_t registrador, uint16_t tamanho, uint8_t* dados);
